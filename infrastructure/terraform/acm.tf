1b82351f7474cd3f99323c5ef0ba6d91e0a16d1e
abhi-octobit8 <abhishek.srivastava@octobit8.com>
2025-06-12 00:01:33 +0530

    Removed duplicate creation of certificate

--- infrastructure/terraform/acm.tf
+++ infrastructure/terraform/acm.tf
@@ -1,20 +1,21 @@
-resource "aws_acm_certificate" "msh_alb_cert" {
-  domain_name       = var.alb_domain_name
-  validation_method = "DNS"
-
-  tags = {
-    Name        = "msh-alb-cert"
-    Environment = "development"
-    project     = "multi-speciality-hospital"
-    owner       = "devops-team"
-    email       = "abhishek.srivastava@octobit8.com"
-    Type        = "acm-certificate"
-  }
-}
+# Comment out or remove the ACM certificate resource to avoid creating a new certificate for the same domain
+# resource "aws_acm_certificate" "msh_alb_cert" {
+#   domain_name       = var.alb_domain_name
+#   validation_method = "DNS"
+#
+#   tags = {
+#     Name        = "msh-alb-cert"
+#     Environment = "development"
+#     project     = "multi-speciality-hospital"
+#     owner       = "devops-team"
+#     email       = "abhishek.srivastava@octobit8.com"
+#     Type        = "acm-certificate"
+#   }
+# }
 
 resource "aws_route53_record" "msh_alb_cert_validation" {
   for_each = {
-    for dvo in aws_acm_certificate.msh_alb_cert.domain_validation_options : dvo.domain_name => {
+    for dvo in data.aws_acm_certificate.msh_alb_cert.domain_validation_options : dvo.domain_name => {
       name   = dvo.resource_record_name
       type   = dvo.resource_record_type
       record = dvo.resource_record_value
