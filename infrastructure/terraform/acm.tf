1de2d9bc8222ef4ae5bd91043915ceb9513929af
abhi-octobit8 <abhishek.srivastava@octobit8.com>
2025-06-11 21:46:38 +0530

    fixed error

--- infrastructure/terraform/acm.tf
+++ infrastructure/terraform/acm.tf
@@ -12,11 +12,6 @@ resource "aws_acm_certificate" "msh_alb_cert" {
   }
 }
 
-resource "aws_acm_certificate_validation" "msh_alb_cert_validation" {
-  certificate_arn         = aws_acm_certificate.msh_alb_cert.arn
-  validation_record_fqdns = [for record in aws_route53_record.msh_alb_cert_validation : record.fqdn]
-}
-
 resource "aws_route53_record" "msh_alb_cert_validation" {
   for_each = {
     for dvo in aws_acm_certificate.msh_alb_cert.domain_validation_options : dvo.domain_name => {
@@ -30,4 +25,10 @@ resource "aws_route53_record" "msh_alb_cert_validation" {
   type    = each.value.type
   records = [each.value.record]
   ttl     = 60
+}
+
+resource "aws_acm_certificate_validation" "msh_alb_cert_validation" {
+  certificate_arn         = aws_acm_certificate.msh_alb_cert.arn
+  validation_record_fqdns = [for record in aws_route53_record.msh_alb_cert_validation : record.fqdn]
+  depends_on              = [aws_route53_record.msh_alb_cert_validation]
 }
