c354756d6df0e0d1cbd1879d1047dcd72ad5dc68
abhi-octobit8 <abhishek.srivastava@octobit8.com>
2025-06-11 09:53:02 +0530

    configured  terraform backend with S3

--- infrastructure/terraform/providers.tf
+++ infrastructure/terraform/providers.tf
@@ -11,3 +11,12 @@ provider "aws" {
   region = "us-east-1"
 }
 
+# Configure S3 bucket for terraform state
+terraform {
+  backend "s3" {
+    bucket         = "msh-terraform-state-dev"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "msh-terraform-lock"
+  }
+}
