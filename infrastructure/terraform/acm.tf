bd3c252796231b44b6b397a7a1d969a01c252630
abhi-octobit8 <abhishek.srivastava@octobit8.com>
2025-06-11 22:01:05 +0530

    Fixed state lock

--- infrastructure/terraform/acm.tf
+++ infrastructure/terraform/acm.tf
@@ -31,4 +31,8 @@ resource "aws_acm_certificate_validation" "msh_alb_cert_validation" {
   certificate_arn         = aws_acm_certificate.msh_alb_cert.arn
   validation_record_fqdns = [for record in aws_route53_record.msh_alb_cert_validation : record.fqdn]
   depends_on              = [aws_route53_record.msh_alb_cert_validation]
+  lifecycle {
+    create_before_destroy = true
+    ignore_changes        = [validation_record_fqdns]
+  }
 }
