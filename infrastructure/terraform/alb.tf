cff03593c7b0e781b05c023de74372e1c84afa0f
abhi-octobit8 <abhishek.srivastava@octobit8.com>
2025-06-15 11:10:23 +0530

    Updated listeners to alb

--- infrastructure/terraform/alb.tf
+++ infrastructure/terraform/alb.tf
@@ -43,9 +43,25 @@ resource "aws_lb_target_group" "msh_alb_tg" {
   }
 }
 
-resource "aws_lb_listener" "msh_alb_listener" {
+resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.msh_alb.arn
-  port              = 443 # or 443 for HTTPS
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
+resource "aws_lb_listener" "https" {
+  load_balancer_arn = aws_lb.msh_alb.arn
+  port              = 443
   protocol          = "HTTPS"
   ssl_policy        = "ELBSecurityPolicy-2016-08"
   certificate_arn   = var.alb_certificate_arn
